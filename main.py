1baf45baf3818cf73766b506d9d4acc62128d073
dog <81295020+Dog-gamer124@users.noreply.github.com>
2021-10-07 13:49:19 +0530

    Basic error handler

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ bot = commands.Bot(
     case_insensitive=True
 )
 
+# Error handler
+@bot.event
+async def on_command_error(ctx, err):
+    if isinstance(err, commands.NotOwner):
+        await ctx.reply("You do not have the needed permissions to execute this command!")
+    elif isinstance(err, commands.MissingPermissions):
+        await ctx.reply("You do not have the needed permissions to execute this command!")
+    else:
+        try:
+            embed = discord.Embed(title="An error occurred!", description=f"{err}", colour=0xCC1818)
+            await ctx.send(embed=embed)
+        except Exception as e:  # If bot can't send error message
+            print("Couldn't send error message!")
+            print(e)
 
 @bot.event
 async def on_ready():
